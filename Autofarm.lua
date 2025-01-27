e266a2188124de089fd0c08198ca69e1b3126ce2
vbjs <108487957+vbjs@users.noreply.github.com>
2025-01-27 19:44:26 +0200

    Update Autofarm.lua

--- Autofarm.lua
+++ Autofarm.lua
@@ -1,4 +1,5 @@
-task.wait(20)
+repeat task.wait() until game:IsLoaded()
+task.wait(8)
 
 getgenv().Config = {
   ["Enabled"] = true,
@@ -16,10 +17,23 @@ asclabel = plr.PlayerGui.PlayerUI.Settings.Progression.ScrollingFrame.Ascension.
 local ascendremote = game.ReplicatedStorage.Remotes.Ascend
 local renew = game.ReplicatedStorage.Remotes.Renew
 local loadlayout = game.ReplicatedStorage.Remotes.LoadLayout
+local Dir = game:GetService("CoreGui"):FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+time = 0
+
+
+Dir.DescendantAdded:Connect(function(Err)
+    if Err.Name == "ErrorTitle" then
+        Err:GetPropertyChangedSignal("Text"):Connect(function()
+            if Err.Text:sub(0, 12) == "Disconnected" then
+               rejoin()
+           end
+       end)
+    end
+end)
 
 local args = {[1] = 1}
 game:GetService("ReplicatedStorage").Remotes.LoadSlot:FireServer(unpack(args))
-task.wait(10)
+task.wait(7)
 local todel = game:GetService("Players").LocalPlayer.PlayerGui.Intro
 todel:Destroy()
 
@@ -41,11 +55,10 @@ game:GetService("TeleportService"):Teleport(game.PlaceId, plr)
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, plr)
 end
 
--- MAIN LOOP --
-
 -- SKIP LABEL DETECTION -- 
 game:GetService("RunService").Heartbeat:Connect(function()
-    task.wait()
+    task.wait(1)
+    time = time+1
     if skiplabel then
       skip = skiplabel.Text
     else
@@ -62,26 +75,29 @@ game:GetService("RunService").Heartbeat:Connect(function()
       if asclabel then asc = asclabel.Text end
     asc = asclabel.Text
     end
+    if time >= 1800 then
+       rejoin()
+    end
 end)
 
 -- MAIN LOOP --
 while task.wait() do
   if Config.Enabled then
-    if plr.Data.Renewal.Value < 250 then
+    if plr.Data.Renewal.Value < 100000 then
       task.wait(2)
-      local args = {[1] = Config.Third}
+      local args = {[1] = Config.Second}
       loadlayout:FireServer(unpack(args))
       repeat task.wait() until tonumber(string.match(skip, "%d+")) > Config.MinSkip
       renew:FireServer()
       plr:WaitForChild("PlayerGui"):WaitForChild("PlayerUI"):WaitForChild("Settings"):WaitForChild("Progression"):WaitForChild("ScrollingFrame"):WaitForChild("Renewal"):WaitForChild("LifeSkip").Text = "1"
-    elseif plr.Data.Renewal.Value >= 250 and plr.Data.Renewal.Value < 750 then
+    elseif plr.Data.Renewal.Value >= 100000 and plr.Data.Renewal.Value < 9999999999999999999999 then
       task.wait(2)
       local args = {[1] = Config.First}
       loadlayout:FireServer(unpack(args))
       repeat task.wait() until tonumber(string.match(skip, "%d+")) > Config.MinSkip
       renew:FireServer()
       plr:WaitForChild("PlayerGui"):WaitForChild("PlayerUI"):WaitForChild("Settings"):WaitForChild("Progression"):WaitForChild("ScrollingFrame"):WaitForChild("Renewal"):WaitForChild("LifeSkip").Text = "1"
-    elseif plr.Data.Renewal.Value >= 750 then
+    elseif plr.Data.Renewal.Value >= 9999999999999999999999 then
       task.wait(2)
       local args = {[1] = Config.Second}
       loadlayout:FireServer(unpack(args))
